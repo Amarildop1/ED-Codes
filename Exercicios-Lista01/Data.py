e5408dde4d2098b82131ee3a9cdd614824ee9610
Amarildo <amarildop1dev@gmail.com>
2020-05-19 23:21:24 -0300

    L01 - Q01

--- Exercicios-Lista01/Data.py
+++ Exercicios-Lista01/Data.py
@@ -4,32 +4,31 @@
 
 class Data:
     def __init__(self, dia, mes, ano):
-        self._dia = dia
-        self._mes = mes
-        self._ano = ano
+        self.__dia = dia
+        self.__mes = mes
+        self.__ano = ano
     
     def get_dia():
-        return _dia
+        return __dia
     
-    def set_dia(self, d):
-        self._dia = d
+    def set_dia(self, dia):
+        self.__dia = dia
 
     def get_mes():
-        return self._mes
+        return self.__mes
 
-    def set_mes(self, m):
-        self._dia = m
+    def set_mes(self, mes):
+        self.__mes = mes
 
     def get_ano():
-        return self._ano
+        return self.__ano
     
-    def set_ano(self, a):
-        self._dia = a
+    def set_ano(self, ano):
+        self.__ano = ano
 
 
     def __str__(self):
-        return f"{self._dia}/{self._mes}/{self._ano}"
-
+        return f"{self.__dia}/{self.__mes}/{self.__ano}"
 
 # Programa Principal
 data1 = Data(1, 1, 2020)
@@ -37,3 +36,12 @@ data2 = Data(19, 5, 2020)
 
 print(data1)
 print(data2)
+
+
+data1.set_dia(31)
+data1.set_mes(10)
+data1.set_ano(2021)
+
+
+print("\n--- Alterei ---\n")
+print(data1)
